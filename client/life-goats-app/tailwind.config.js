171f016cb08adb25f6e5471764194561436d4988
Anastasia Adamoudi <53370412+AnastasiaAdamoudi@users.noreply.github.com>
2023-10-26 23:40:05 +0100

    Tailwind colors and navbar draft added

--- client/life-goats-app/tailwind.config.js
+++ client/life-goats-app/tailwind.config.js
@@ -1,9 +1,43 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
-  theme: {
-    extend: {},
+export const content = ["./index.html",
+"./src/**/*.{js,ts,jsx,tsx}"];
+export const theme = {
+  extend: {
+    fontFamily: {
+      title: ['League Spartan'],
+      body: ['Roboto'],
+      links: ['DM Sans'],
+      fallbacks: {
+        title: ['Roboto', 'Arial', 'sans-serif'],
+        body: ['Roboto', 'Arial', 'sans-serif'],
+        links: ['Roboto', 'Arial', 'sans-serif']
+      }
+    },
+    colors: {
+      background: "#C7D1D8",
+      primaryT:  "#303848",
+      secondaryT: "#607DFA",
+      tertiaryT: "#94B766",
+      "black-100": "#100d25",
+      "black-200": "#090325",
+      "white-100": "#f3f3f3",
+    },
+    boxShadow: {
+      title: "0px 4px 4px rgba(0, 0, 0, 0.25)",
+      button: "0px 4px 4px rgba(0, 0, 0, 0.25)"
+    },
+    screens: {
+      'xs': '485px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px'
+    },
+    backgroundImage: {
+      // "wizard-hat": "url('/src/assets/wizard-hat.png')",
+      // "magic": "url('/src/assets/magic.png')",
+    }
   },
-  plugins: [],
-}
+};
+export const plugins = [];
 
