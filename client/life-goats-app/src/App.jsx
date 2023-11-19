13d44344efc772c046772c7b7aafa44ecdc20c8c
Anastasia Adamoudi <53370412+AnastasiaAdamoudi@users.noreply.github.com>
2023-11-19 22:28:36 +0000

    Homepage router set up

--- client/life-goats-app/src/App.jsx
+++ client/life-goats-app/src/App.jsx
@@ -1,10 +1,25 @@
-import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
+import Hero from './components/Hero/Hero';
+import About from './components/About/About';
+import Program from './components/Program/Program';
+import Contact from './components/Contact/Contact';
+import Footer from './components/Footer/Footer';
 
 function App() {
 
   return (
     <div className="App">
       <Navbar />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Hero />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/program" element={<Program />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Router>
+      <Footer />
     </div>
   )
 }
