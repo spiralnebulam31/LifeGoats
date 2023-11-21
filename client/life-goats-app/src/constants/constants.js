c3b69076f362a1033f0d01ad0b85d9861a364281
Anastasia Adamoudi <53370412+AnastasiaAdamoudi@users.noreply.github.com>
2023-11-21 22:40:37 +0000

    Prepared styles.js and constants.js files

---
+++ client/life-goats-app/src/constants/constants.js
@@ -0,0 +1,95 @@
+import {
+    accommodationIcon,
+    altitudeIcon,
+    durationIcon,
+    elementsIcon,
+    groupIcon,
+    goat,
+    nature,
+    peace,
+    refuge,
+    } from '../assets';
+    
+export const navLinks = [
+    {
+      id: "about",
+      title: "About",
+    },
+    {
+      id: "program",
+      title: "Program",
+    },
+    {
+      id: "location",
+      title: "location",
+    },
+    {
+      id: "contact",
+      title: "Contact",
+    },
+  ];
+
+  const details = [
+    {
+      name: "Accomodation",
+      icon: accommodationIcon,
+      alt: "accommodation icon",
+      text: ""
+    },
+    {
+        name: "Altitude",
+        icon: altitudeIcon,
+        alt: "altitude icon",
+        text: ""
+    },
+    {
+        name: "Duration",
+        icon: durationIcon,
+        alt: "duration icon",
+        text: ""
+      },
+      {
+        name: "Elements",
+        icon: elementsIcon,
+        alt: "elements icon",
+        text: ""
+      },
+      {
+        name: "Group",
+        icon: groupIcon,
+        alt: "group icon",
+        text: ""
+      }
+    ]
+    
+
+  const program = [
+    {
+      index: 0,
+      alt: "goat",
+      image: goat,
+      title: "",
+      text: ""
+    },
+    {
+      index: 1,
+      alt: "nature",
+      image: nature,
+      title: "",
+      text: ""
+    },
+    {
+      index: 2,
+      alt: "peace",
+      image: peace,
+      title: "",
+      text: ""
+    },
+    {
+      index: 3,
+      alt: "refuge",
+      image: refuge,
+      title: "",
+      text: ""
+    }
+  ];
