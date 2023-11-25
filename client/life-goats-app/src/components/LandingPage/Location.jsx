27894fdb1b556d4d0ed75aacf58c2dee4ff44091
Anastasia Adamoudi <53370412+AnastasiaAdamoudi@users.noreply.github.com>
2023-11-25 08:23:42 +0000

    Leaflet is working, showing the desired map in Location

--- client/life-goats-app/src/components/LandingPage/Location.jsx
+++ client/life-goats-app/src/components/LandingPage/Location.jsx
@@ -1,14 +1,30 @@
-import {
-  MapContainer,
-  TileLayer,
-  useMap,
-} from 'https://cdn.esm.sh/react-leaflet'
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+// import dotenv from 'dotenv';
+// dotenv.config();
 
 const Location = () => {
+
+  //   latitude: 40.102947,
+  //   longitude: 22.502612,
+
+
   return (
     <section id="location" className="overflow-hidden">
     <div className="bg-background relative bg-cover w-full h-screen top-0 left-0 right-0 bottom-0 pt-5 pb-10">
     <h1>Location</h1>
+
+    <MapContainer center={[40.102947, 22.502612]} zoom={13} scrollWheelZoom={false} className="w-full h-full">
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[40.102947, 22.502612]}>
+        <Popup>
+          Mount Olympus
+        </Popup>
+      </Marker>
+    </MapContainer>
+
     </div>
     </section>
   )
