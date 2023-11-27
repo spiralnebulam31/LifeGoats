b6d68bcf726a9aabd37a74c14c32532b8dbad053
Anastasia Adamoudi <53370412+AnastasiaAdamoudi@users.noreply.github.com>
2023-11-27 11:23:58 +0000

    Updated Location info (moved data to constants), added section title, updated styling (now fully responsive)

--- client/life-goats-app/src/constants/constants.js
+++ client/life-goats-app/src/constants/constants.js
@@ -112,7 +112,22 @@ export const locationData = [
     id: 2,
     name: "Mount Olympus",
     description: "Located in the northern part of mainland Greece, it's the mythical residence of the Greek Gods and currently a treasured hiking destination. Thanks to its high altitude, you can experience a variety of landscapes, from lush green forests and clear water streams to rocky terrains that resemble alpine scenery.",
-    lat: 40.07974974094711,
-    lng: 22.387254365241404,
+    lat: 40.06677,
+    lng: 22.40607,
   },
+];
+
+export const locationDataInfo = [
+  {
+    paragraph: "Located in the northern part of mainland Greece, Mount Olympus is the mythical residence of the Greek Gods and currently a nature reserve and treasured hiking destination. Thanks to its high altitude, you can experience a variety of landscapes, from lush green forests and clear water streams to rocky terrains that resemble alpine scenery."
+  },
+  {
+    paragraph: "At the foothill of Mount Olympus, the town of Litochoro is home to quaint streets and traditional food. The views of the mountain are captivating, a timeless spectacle throughout the year. Nature and culture co-exist in perfect harmony in this peaceful place."
+  },
+  {
+    paragraph: "Last but not least, Plaka, a settlement by the sea, offers long beaches and clear waters suitable for swimming, only a few kilometers away from Litochoro and Mount Olympus. You can also find a plethora of food choices in taverns and restaurants."
+  },
+  {
+    googleMapsLink: "https://maps.app.goo.gl/RMdWS12Tge9Jnxot5"
+  }
 ];
