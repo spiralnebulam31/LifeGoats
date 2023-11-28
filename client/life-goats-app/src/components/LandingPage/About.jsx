600e0e210b4b235ab895afd4228ba2dc5aed474d
Anastasia Adamoudi <53370412+AnastasiaAdamoudi@users.noreply.github.com>
2023-11-28 09:26:40 +0000

    Added info in About and made it fully responsive

--- client/life-goats-app/src/components/LandingPage/About.jsx
+++ client/life-goats-app/src/components/LandingPage/About.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import { motion } from 'framer-motion';
+import { aboutText } from '../../constants/constants';
 
 const About = () => {
   return (
-    <section id="about" className="overflow-hidden">
-    <div className="bg-gradient-to-b from-background via-[#96beeb] to-[#2F6FB7] relative bg-cover w-full h-screen top-0 left-0 right-0 bottom-0 pt-5 pb-10">
-      <h1>About</h1>
-    </div>
+    <section id="about" className="overflow-hidden bg-gradient-to-b from-background via-[#96beeb] to-[#2F6FB7] relative bg-cover w-full h-screen top-0 left-0 right-0 bottom-0 pt-5 pb-10">
+      
+      <motion.div className="text-start w-[80%] lg:w-[90%] mx-auto lg:mr-4 lg:ml-[15%]
+        pt-6 mb-5 z-10 overflow-hidden">
+          <p className="md:text-[18px] text-[14px] text-secondary font-subtitle
+          font-bold uppercase tracking-wider">
+            Maria's reflections
+          </p>
+          <h2 className="text-earth font-bold font-title uppercase md:text-[55px]
+          sm:text-[45px] text-[40px] outline-background-2 pb-4 mt-0"
+          style={{ textShadow: "2px 2px 3px rgba(0,0,0,0.3)" }}>
+            About The Retreat
+          </h2>
+        </motion.div>
+
+        <motion.div className="flex flex-col justify-center items-center mx-auto bg-gradient-to-b from-secondary via-tertiary to-earth p-2 m-3 w-[55%] rounded-2xl shadow-card">
+        <div className="w-full h-auto py-4 bg-background rounded-2xl">
+        {aboutText.map((text) => (
+          <div key={text.index} className="w-full px-10 py-3">
+            {text.paragraph && <p className="md:text-[16px] text-[14px] font-body text-primary">{text.paragraph}</p>}
+            {text.endText && <p className="md:text-[18px] text-[16px] font-bold font-body text-primary">{text.endText}</p>}
+          </div>
+        ))}
+        </div>
+        </motion.div>
+
+
 
     </section>
   )
--- client/life-goats-app/src/constants/constants.js
+++ client/life-goats-app/src/constants/constants.js
@@ -26,8 +26,35 @@ export const navLinks = [
     title: "Location",
   },
   {
+    id: "program",
+    title: "Program",
+  },
+  {
     id: "contact",
     title: "Contact",
+  },
+];
+
+export const aboutText = [
+  {
+    paragraph:
+      "As professionals in people centered services, we are always learning & evolving.",
+  },
+  {
+    paragraph:
+      "Oftentimes in our efforts to share our learnings, to be the stable pilar for and help others, we lose our center.",
+  },
+  {
+    paragraph:
+      "As a newer coach I watch myself and many of my colleagues go off the rails. Trying to build our business and contribute meaningfully with our offers and services while remaining authentic and staying true to our values. It’s a challenge to keep the balance and we often feel so isolated in our efforts in this digital age.",
+  },
+  {
+    paragraph:
+      "We deserve to recharge our batteries, be in community and reinvent ourselves. In fact, our performance in the long run depends on that.",
+  },
+  {
+    endText:
+      "So come on over and spend some time climbing Mt. Olympus with us.",
   },
 ];
 
@@ -106,28 +133,31 @@ export const overviewData = [
     image: hikers,
     title: "Exclusivity",
     text: "The retreat is invite-only, offering you the opportunity to engage with a remarkable and diverse group of like-minded individuals",
-  }
+  },
 ];
 
 export const locationData = [
   {
     id: 0,
     name: "Litochoro",
-    description: "At the foothill of Mount Olympus, this town is home to quaint streets and traditional food. The views of the mountain are captivating, a timeless spectacle throughout the year. Nature and culture co-exist in perfect harmony in this peaceful place.",
+    description:
+      "At the foothill of Mount Olympus, this town is home to quaint streets and traditional food. The views of the mountain are captivating, a timeless spectacle throughout the year. Nature and culture co-exist in perfect harmony in this peaceful place.",
     lat: 40.105421322419744,
     lng: 22.502369043519394,
   },
   {
     id: 1,
     name: "Plaka",
-    description: "Settlement by the sea, offering long beaches and clear waters suitable for swimming, only a few kilometers away from Litochoro and Mount Olympus. You can also find a plethora of food choices in taverns and restaurants.",
+    description:
+      "Settlement by the sea, offering long beaches and clear waters suitable for swimming, only a few kilometers away from Litochoro and Mount Olympus. You can also find a plethora of food choices in taverns and restaurants.",
     lat: 40.10282210911825,
     lng: 22.562262661060828,
   },
   {
     id: 2,
     name: "Mount Olympus",
-    description: "Located in the northern part of mainland Greece, it's the mythical residence of the Greek Gods and currently a treasured hiking destination. Thanks to its high altitude, you can experience a variety of landscapes, from lush green forests and clear water streams to rocky terrains that resemble alpine scenery.",
+    description:
+      "Located in the northern part of mainland Greece, it's the mythical residence of the Greek Gods and currently a treasured hiking destination. Thanks to its high altitude, you can experience a variety of landscapes, from lush green forests and clear water streams to rocky terrains that resemble alpine scenery.",
     lat: 40.06677,
     lng: 22.40607,
   },
@@ -135,15 +165,18 @@ export const locationData = [
 
 export const locationDataInfo = [
   {
-    paragraph: "Located in the northern part of mainland Greece, Mount Olympus is the mythical residence of the Greek Gods and currently a nature reserve and treasured hiking destination. Thanks to its high altitude, you can experience a variety of landscapes, from lush green forests and clear water streams to rocky terrains that resemble alpine scenery."
+    paragraph:
+      "Located in the northern part of mainland Greece, Mount Olympus is the mythical residence of the Greek Gods and currently a nature reserve and treasured hiking destination. Thanks to its high altitude, you can experience a variety of landscapes, from lush green forests and clear water streams to rocky terrains that resemble alpine scenery.",
   },
   {
-    paragraph: "At the foothill of Mount Olympus, the town of Litochoro is home to quaint streets and traditional food. The views of the mountain are captivating, a timeless spectacle throughout the year. Nature and culture co-exist in perfect harmony in this peaceful place."
+    paragraph:
+      "At the foothill of Mount Olympus, the town of Litochoro is home to quaint streets and traditional food. The views of the mountain are captivating, a timeless spectacle throughout the year. Nature and culture co-exist in perfect harmony in this peaceful place.",
   },
   {
-    paragraph: "Last but not least, Plaka, a settlement by the sea, offers long beaches and clear waters suitable for swimming, only a few kilometers away from Litochoro and Mount Olympus. You can also find a plethora of food choices in taverns and restaurants."
+    paragraph:
+      "Last but not least, Plaka, a settlement by the sea, offers long beaches and clear waters suitable for swimming, only a few kilometers away from Litochoro and Mount Olympus. You can also find a plethora of food choices in taverns and restaurants.",
   },
   {
-    googleMapsLink: "https://maps.app.goo.gl/RMdWS12Tge9Jnxot5"
-  }
+    googleMapsLink: "https://maps.app.goo.gl/RMdWS12Tge9Jnxot5",
+  },
 ];
