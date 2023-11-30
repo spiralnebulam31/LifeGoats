2c84957e925b9bd890fe83006e530f55839df747
Anastasia Adamoudi <53370412+AnastasiaAdamoudi@users.noreply.github.com>
2023-11-30 00:03:21 +0000

    Mobile menu working, styled and responsive

--- client/life-goats-app/src/components/Navbar/Navbar.jsx
+++ client/life-goats-app/src/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
+import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
-import { styles } from "../../styles.js";
 import { navLinks } from "../../constants/constants";
 import { lifeGoatsLogo, menuMountain, menuClose } from "../../assets";
 
@@ -9,11 +9,17 @@ const Navbar = () => {
   const [mobile, setMobile] = useState(false);
 
   return (
-     
-    <nav className={"fixed w-full top-0 z-50 bg-background shadow-xl pr-8 pl-5 py-2"}>
-
-      <div className="flex w-full mx-auto justify-between gap-20"> {/* beginning of flex div */}
-        <div className="flex items-center justify-start"> {/* beginning of left side div */}
+    <nav
+      className={
+        "fixed w-full top-0 z-50 bg-background shadow-xl pr-8 pl-5 py-2"
+      }
+    >
+      <div className="flex w-full mx-auto justify-between gap-20">
+        {" "}
+        {/* beginning of flex div */}
+        <div className="flex items-center justify-start">
+          {" "}
+          {/* beginning of left side div */}
           <Link
             to="/"
             className="flex items-center gap-2"
@@ -31,36 +37,77 @@ const Navbar = () => {
               Life Goats
             </p>
           </Link>
-        </div>  {/* end of left side div */}
-
-        <div className="flex items-center justify-end"> {/* beginning of right side div */}
+        </div>{" "}
+        {/* end of left side div */}
+        <div className="flex items-center justify-end">
+          {" "}
+          {/* beginning of right side div */}
           <ul className="list-none hidden lg:flex flex-row gap-10">
             {navLinks.map((link) => (
               <li
                 key={link.id}
                 className={`${
-                  active === link.id ? "text-secondary underline" : "text-primary"
+                  active === link.id
+                    ? "text-secondary underline"
+                    : "text-primary"
                 } hover:text-tertiary text-[20px] font-links font-bold cursor-pointer`}
                 onClick={() => {
                   setActive(link.id);
                   window.scrollTo(0, 0);
-                }}      
+                }}
               >
                 <a href={`#${link.id}`}>{link.title}</a>
               </li>
             ))}
           </ul>
-
-          <div className="lg:hidden flex flex-1 justify-end items-center gap-2">
+          {/* mobile menu button */}
+          <motion.div>
+          <div className="lg:hidden flex flex-1 justify-end items-center gap-">
             <img
               src={mobile ? menuClose : menuMountain}
               alt="menu"
-              className="w-16 h-auto cursor-pointer object-contain"
+              className="w-16 h-auto cursor-pointer object-contain z-30"
               onClick={() => setMobile(!mobile)}
             />
           </div>
-        </div> {/* end of right side div */}
-      </div> {/* end of flex div */}
+          {/* mobile menu */}
+          
+          <motion.div
+            className={`${!mobile ? "hidden" : "flex"}
+              absolute right-0 top-0 bottom-0 w-[50%] h-screen mx-0 my-0
+              z-10 flex-col items-end gap-60 bg-gradient-to-b from-secondary via-tertiary to-earth shadow-lg rounded-tl-xl rounded-bl-xl`}
+            initial={mobile ? "open" : "closed"}
+            animate={mobile ? "open" : "closed"}
+          >
+            
+              <ul className="list-none font-links font-bold flex flex-col justify-center text-center items-center gap-6 bg-background px-16 rounded-tl-xl rounded-bl-xl w-[90%] h-screen">
+                {navLinks.map((link) => (
+                  <motion.div key={link.id}>
+                    <li
+                      key={link.id}
+                      className={`${
+                        active === link.title
+                          ? "text-secondary underline"
+                          : "text-primary"
+                      } hover:text-tertiary text-[20px] hover:text-[24px] font-medium cursor-pointer leading-7 w-[86px]`}
+                      onClick={() => {
+                        setMobile(!mobile);
+                        setActive(link.title);
+                        window.scrollTo(0, 0);
+                      }}
+                    >
+                      <a href={`#${link.id}`}>{link.title}</a>
+                    </li>
+                  </motion.div>
+                ))}
+              </ul>
+            </motion.div>
+          </motion.div>
+          {/* end of mobile menu */}
+        </div>
+        {/* end of mobile menu button */}
+      </div>
+      {/* end of right side div */}
     </nav>
   );
 };
