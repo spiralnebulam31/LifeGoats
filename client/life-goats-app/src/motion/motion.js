961a1c4938328bf1a5298468d7b6eb87b69d1e92
Anastasia Adamoudi <53370412+AnastasiaAdamoudi@users.noreply.github.com>
2023-12-26 12:19:17 +0000

    Separated Hero variants, fixed responsiveness details in Hero, adjusted Hero animation reveal

--- client/life-goats-app/src/motion/motion.js
+++ client/life-goats-app/src/motion/motion.js
@@ -11,48 +11,4 @@ export const fadeIn = {
         staggerChildren: 1
       },
     },
-    }
-
-export const heroVariant = {
-    initial: {
-      y:-500,
-      opacity: 0,
-    },
-    animate: {
-      y: 0,
-      opacity: 1,
-      transition: {
-        duration: 1,
-        delay: 0.5,
-      },
-    },
-  }
-
-  export const heroGoatPrintsVariant = {
-    initial: {
-      y: 500,
-      opacity: 0,
-    },
-    animate: {
-      y: 0,
-      opacity: 1,
-      transition: {
-        duration: 1,
-        delay: 0.5,
-      },
-    },
-  }
-
-
-  export const goatPrintsSliderVariant = {
-    initial: {
-      x: 0
-    },
-  animate: {
-        y: [0, 20, 0],
-    transition: {
-      duration: 3,
-      repeat: Infinity,
-      repeatType: "loop",
-    },
-  }}
+    };
