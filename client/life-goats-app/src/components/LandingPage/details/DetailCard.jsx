67170f1d2d0c1e2a2b5609fac4bb68de3db00a27
Anastasia Adamoudi <53370412+AnastasiaAdamoudi@users.noreply.github.com>
2024-01-27 23:51:37 +0000

    Added interval in Details to display the numbers increasing

--- client/life-goats-app/src/components/LandingPage/details/DetailCard.jsx
+++ client/life-goats-app/src/components/LandingPage/details/DetailCard.jsx
@@ -1,27 +1,46 @@
 import { motion } from 'framer-motion';
+import { useEffect, useState } from 'react';
 
-const DetailCard = ({ index, name, icon, alt, text }) => {
+const DetailCard = ({ index, name, icon, alt, text, text2, initialNumber }) => {
+    const [currentNumber, setCurrentNumber] = useState(initialNumber);
+    const targetNumber = parseInt(text);
+
+    useEffect(() => {
+        if (!isNaN(targetNumber)) {
+            const interval = setInterval(() => {
+                setCurrentNumber(prevNumber => {
+                    if (prevNumber < targetNumber) {
+                        return prevNumber + 1;
+                    } else {
+                        clearInterval(interval); // Stop the interval when reaching the target
+                        return prevNumber;
+                    }
+                });
+            }, 100); // adjust speed as needed
+
+            return () => clearInterval(interval);
+        }
+    }, [targetNumber]);
+
     return (
-      <motion.div
-      className="bg-background rounded-2xl flex justify-center items-center flex-col"
-    >
-      <motion.div
-       className="bg-background rounded-2xl flex justify-evenly items-center flex-row p-1"
-          >
-            <img
-              src={icon}
-              alt={alt}
-              className="w-18 lg:w-20 object-contain rounded-2xl hue-rotate-180 lg:h-20 p-2 mb-1 shadow-md"
-            />
-      </motion.div>
-          <motion.div
-            className="bg-background rounded-2xl flex justify-evenly items-center flex-col  px-4 lg:px-1 h-20"
-          >
-            <p className="text-tertiary font-subtitle text-center text-[13px] lg:text-[14px] font-bold max-w-6xl -pt-1 -mb-4 px-4 h-10">{name}</p>
-            <p className="text-primary font-body flex flex-wrap text-center text-[15px] lg:text-[16px] w-22 lg:w-24 h-10 justify-center items-start mx-auto">{text}</p>
-          </motion.div>
+        <motion.div className="bg-background rounded-2xl flex justify-center items-center flex-col">
+            <motion.div className="bg-background rounded-2xl flex justify-evenly items-center flex-row p-1">
+                <img
+                    src={icon}
+                    alt={alt}
+                    className="w-18 lg:w-20 object-contain rounded-2xl hue-rotate-180 lg:h-20 p-2 mb-1 shadow-md"
+                />
+            </motion.div>
+            <motion.div className="bg-background rounded-2xl flex justify-evenly items-center flex-col  px-4 lg:px-1 h-20">
+                <p className="text-tertiary font-subtitle text-center text-[13px] lg:text-[14px] font-bold max-w-6xl -pt-1 -mb-4 px-4 h-10">
+                    {name}
+                </p>
+                <p className="text-primary font-body font-bold flex flex-wrap text-center text-[15px] lg:text-[16px] w-22 lg:w-24 h-10 justify-center items-start mx-auto">
+                    {isNaN(targetNumber) ? text : currentNumber}{text2}
+                </p>
+            </motion.div>
         </motion.div>
     );
-  };
+};
 
 export default DetailCard;
