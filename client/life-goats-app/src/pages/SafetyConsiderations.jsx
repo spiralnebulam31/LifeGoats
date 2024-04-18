b70a86f80f31336100e377e63e7b43b6a88ad353
Anastasia Adamoudi <53370412+AnastasiaAdamoudi@users.noreply.github.com>
2024-04-18 11:00:45 +0100

    Fixed detail in safety considerations

--- client/life-goats-app/src/pages/SafetyConsiderations.jsx
+++ client/life-goats-app/src/pages/SafetyConsiderations.jsx
@@ -6,7 +6,7 @@ import { safetyConsiderationsBG } from "../assets";
 const SafetyConsiderations = () => {
   return (
     <motion.div
-      className="bg-background relative bg-cover w-screen h-auto min-h-full inset-0 left-0 right-0 top-0 overflow-hidden pt-2 pb-2 z-10 mb-[130px] flex flex-col justify-center text-center mx-auto"
+      className="bg-background relative bg-cover w-screen h-auto min-h-screen inset-0 left-0 right-0 top-0 overflow-hidden pt-2 pb-2 z-10 mb-[130px] flex flex-col justify-center text-center mx-auto"
     >
       <img
         src={safetyConsiderationsBG}
@@ -22,29 +22,7 @@ const SafetyConsiderations = () => {
             for a safe hike
           </motion.h2>
         </motion.div>
-        <motion.div className="bg-background flex flex-col justify-center text-center mx-auto pt-10 pb-8 font-body lg:text-lg text-md w-[80%] lg:w-[70%]" variants={fadeIn} initial="initial" whileInView="animate">
-          <p>Here are some safety considerations to keep in mind:</p>
-          <br />
-          <p>1. Check the weather forecast before you go.</p>
-          <br />
-          <p>2. Dress in layers.</p>
-          <br />
-          <p>3. Wear sturdy shoes.</p>
-          <br />
-          <p>4. Bring plenty of water.</p>
-          <br />
-          <p>5. Bring snacks.</p>
-          <br />
-          <p>6. Stay on the trail.</p>
-          <br />
-          <p>7. Be aware of your surroundings.</p>
-          <br />
-          <p>8. Tell someone where you are going.</p>
-          <br />
-          <p>9. Bring a map and compass.</p>
-          <br />
-          <p>10. Be prepared for emergencies.</p>
-        </motion.div>
+        <p className="pb-10">Coming soon!</p>
       </motion.div>
       <div className="relative z-20 w-[80%] lg:w-[70%] mx-auto pt-10 pb-8 font-body lg:text-lg text-md">
         <motion.p className="font-bold pt-14 pb-4 text-center" variants={fadeIn} initial="initial" whileInView="animate">
