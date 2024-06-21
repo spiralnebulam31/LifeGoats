1935df105cb356fa08b56ab29ceeabac680f22ce
Anastasia Adamoudi <53370412+AnastasiaAdamoudi@users.noreply.github.com>
2024-06-21 21:54:14 +0100

    Testimonials page and section ready styled and responsive - not the right content yet

--- client/life-goats-app/src/components/LandingPage/testimonials/Testimonials.jsx
+++ client/life-goats-app/src/components/LandingPage/testimonials/Testimonials.jsx
@@ -30,7 +30,7 @@ const Testimonials = ({ isSmallScreen }) => {
       </motion.div>
 
       <motion.div className="flex flex-row justify-evenly items-center">
-        <motion.div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-10 mb-2 md:mb-4">
+        <motion.div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-4 md:gap-10 mb-2 md:mb-4">
           {testimonials.slice(0,6).map((testimonial, index) => (
             <motion.div
               key={index}
--- client/life-goats-app/src/components/LandingPage/testimonials/TestimonialsCard.jsx
+++ client/life-goats-app/src/components/LandingPage/testimonials/TestimonialsCard.jsx
@@ -3,14 +3,14 @@ import { motion } from 'framer-motion';
 const TestimonialsCard = ({ index, alt, image, name, text, yMoving }) => {
   return (
     <motion.div
-      className="flex flex-row mx-auto gap-1 justify-center text-center my-auto w-[280px] h-[320px] lg:w-[480px] px-4 z-0 bg-background m-3 rounded-2xl shadow-lg overflow-hidden"
+      className="flex flex-row mx-auto gap-1 justify-center text-center my-auto w-[340px] h-[300px] md:w-[400px] px-2 z-0 bg-background m-2 rounded-2xl shadow-lg overflow-hidden"
       >
         <img
           src={image}
           alt={alt}
-          className="flex justify-center text-center my-auto object-cover w-40 h-40 rounded-full drop-shadow-lg"
+          className="flex justify-center text-center my-auto object-cover w-28 h-28 md:w-36 md:h-36 rounded-full drop-shadow-lg"
         />
-        <div className="px-6 pt-3 pb-1 flex flex-col justify-center text-center mx-auto">
+        <div className="px-2 pt-3 pb-1 flex flex-col justify-center text-center mx-auto">
           <p className="text-primary font-body text-center text-[14px] md:text-[16px] py-2">
             {text}
           </p>
