781b1400eed54cf6f754b09de9355430f100a8cf
Anastasia Adamoudi <53370412+AnastasiaAdamoudi@users.noreply.github.com>
2024-07-10 00:31:37 +0100

    Added config for vercel deployment

--- client/life-goats-app/vite.config.js
+++ client/life-goats-app/vite.config.js
@@ -4,5 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.JPG', '**/*.jpg']
+  assetsInclude: ['**/*.JPG', '**/*.jpg'],
+  root: process.cwd(),
+  build: {
+    outDir: './dist',
+    assetsDir: './assets',
+    base: '/',
+  },
 })
