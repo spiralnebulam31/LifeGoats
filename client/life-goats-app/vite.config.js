5d912e96277d14863a98dcd40bf28839b7e4edab
Anastasia Adamoudi <53370412+AnastasiaAdamoudi@users.noreply.github.com>
2024-07-12 22:10:22 +0100

    Updated vite config file to debug

--- client/life-goats-app/vite.config.js
+++ client/life-goats-app/vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.JPG', '**/*.jpg'],
   root: process.cwd(),
+  publicDir: 'public',
   build: {
     outDir: './dist',
     assetsDir: './assets',
