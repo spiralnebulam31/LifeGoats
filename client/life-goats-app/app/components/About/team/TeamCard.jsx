eea7a4138f409fb1bf834bbf67565a1dae070e93
AnastasiaAdamoudi <anastasia@pie.co.de>
2024-12-24 09:37:22 +0000

    Fixed broken image paths

--- client/life-goats-app/app/components/About/team/TeamCard.jsx
+++ client/life-goats-app/app/components/About/team/TeamCard.jsx
@@ -1,5 +1,6 @@
 import { motion } from 'framer-motion';
 import parse, { domToReact } from 'html-react-parser';
+import Image from 'next/image';
 import PropTypes from 'prop-types';
 
 const transform = (node) => {
@@ -19,10 +20,12 @@ const TeamCard = ({ alt, image, title, text, url }) => {
     <motion.div
       className="w-[280px] lg:w-[350px] h-[550px] z-0 bg-background m-3 py-5 rounded-2xl shadow-lg opacity-80 overflow-hidden"
       >
-        <img
+        <Image
           src={image}
           alt={alt}
           className="flex justify-center mx-auto w-52 h-52 object-cover rounded-full"
+          width={400}
+          height={400}
         />
         <div className="px-6 pt-3 pb-1 flex flex-col mx-auto">
         <a href={url} target="_blank" rel="noreferrer">
