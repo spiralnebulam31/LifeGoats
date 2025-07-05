6e0d56a30bd5a77ca5c3b966ee5589f85cabb0c4
AnastasiaAdamoudi <anastasia@pie.co.de>
2025-07-05 22:33:43 +0100

    Updated homepage parallax text

--- client/life-goats-app/app/components/LandingPage/parallax/Parallax.jsx
+++ client/life-goats-app/app/components/LandingPage/parallax/Parallax.jsx
@@ -6,12 +6,13 @@ const Parallax = () => {
   };
 
   return (
-    <section id="parallax" style={sectionStyle} className="bg-gradient-to-b from-background via-blue-100 to-blue-400 bg-contain lg:bg-cover
+    <section id="parallax" style={sectionStyle} className="bg-gradient-to-b from-background via-blue-100 to-blue-400 bg-contain lg:bg-cover 
     bg-no-repeat bg-fixed relative w-screen h-screen pt-10 inset-0 overflow-hidden z-10">
       <div className="relative w-screen h-full">
         <div className="absolute top-0 left-0 w-screen h-screen flex justify-center items-center mx-auto">
-          <h1 className="text-5xl md:text-6xl lg:text-7xl xl:text-8xl text-background font-bold font-links text-center drop-shadow-lg">
-            We are life goats!
+          <h1 className="w-[90%] md:w-[75%] lg:w-[60%] text-3xl md:text-4xl lg:text-5xl xl:text-6xl text-background
+          font-bold font-links text-center drop-shadow-lg text-shadow-lg leading-8">
+          “Give the ones you love wings to fly, roots to come back and reasons to stay.”
           </h1>
         </div>
       </div>
