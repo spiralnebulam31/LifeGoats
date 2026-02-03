977786771c8d23336eceeb2f71ad0c04c0f15a78
Anastasia Adamoudi <anastasiaadamoudi@gmail.com>
2026-02-03 03:19:25 +0000

    Removed old files

--- client/life-goats-app/public/assets/subscriptions/index.js
+++
@@ -1,5 +0,0 @@
-import subscribeImg from "./subscribeImg.jpg";
-import unsubscribeImg from "./unsubscribeImg.jpg";
-import hikingBoots from "./hikingBoots.jpeg";
-
-export { subscribeImg, unsubscribeImg, hikingBoots };
--- client/life-goats-app/public/assets/testimonials/index.js
+++
@@ -1,5 +0,0 @@
-import herd from "./herd.jpg";
-
-export {
-    herd
-};
